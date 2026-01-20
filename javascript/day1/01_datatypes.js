d88a9b4ae5cb292d8eef4862669f5bee7e9ac319
Tejas <tejaspawar8420@gmail.com>
2026-01-20 15:22:49 +0530

    datatypes of javascript

---
+++ javascript/day1/01_datatypes.js
@@ -0,0 +1,18 @@
+let name = "js";
+let age = 15;
+let isLoggenIn = true;
+let number = null;// prints object type
+let place;//undefined
+
+/*lets find their datatypes 
+two types - primitive and non primitive
+primitve = string,boolean,number,symbol
+
+*/
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(isLoggenIn));
+console.log(typeof(number));//null = object
+console.log(typeof(place));//undefined
+
+
