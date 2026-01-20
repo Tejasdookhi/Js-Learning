c2b0278865794ffe68fb3b638eb054608a73ccc0
Tejas <tejaspawar8420@gmail.com>
2026-01-20 14:58:15 +0530

    variables in javascript

---
+++ javascript/day1/01_variables.js
@@ -0,0 +1,23 @@
+//comments in js 
+/* multi line comments in js
+
+ variables = stores some data
+ const , let , and var ,
+ try not to use var beacuse of block scoped and function scoped
+
+*/
+
+const phoneNumber = 123456;
+let id = "javascript"
+var pass = "1234"
+let city ;//undefined nothing declared 
+let value = 0;//null
+
+console.table([phoneNumber, id , pass]);
+
+// phoneNumber = 55475; //const cannot change or reassign.
+id = "java";
+pass = "4321";
+
+console.table([phoneNumber, id , pass , city , value]);
+console.log(typeof(value))
