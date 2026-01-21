d1407e50602af6f17e463bab49fbeecace0bd202
Tejas <tejaspawar8420@gmail.com>
2026-01-21 23:25:36 +0530

    Strings in js

---
+++ javascript/day2/02_string.js
@@ -0,0 +1,19 @@
+//strings in js 
+
+let name = "javascript";
+console.log(name);
+console.log(typeof(name));
+
+let surname = ".js";
+
+//adding both strings
+
+console.log(name  +  surname);
+
+//template literals using ``;
+
+console.log(`My name is ${name} and my surname is ${surname}`);
+
+
+
+
