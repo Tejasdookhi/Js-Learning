5d0f14ae3374bf94f72d3c9a285f4666f923d6cd
Tejas <tejaspawar8420@gmail.com>
2026-01-24 00:00:07 +0530

    math.js

---
+++ javascript/day3/03_math.js
@@ -0,0 +1,69 @@
+console.log(Math);//pbject 
+
+console.log(Math.abs(-4));//4 Math.abs -4 to 4 only for negative
+console.log(Math.round(2.1));//2 round off 
+console.log(Math.round(2.6));//3
+
+
+console.log(Math.ceil(4.1));// 5 ceil top value if 10.1 prints 11
+console.log(Math.floor(9.9));//9 lowest value
+
+console.log(Math.min(1, 2, 3, 4, 5));//1 min
+console.log(Math.max(1, 2, 3, 4, 5));//5 max
+
+console.log(Math.random());//random value bw 0 to 1
+
+
+// random number between given range
+const min = 1;
+const max = 10;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min  );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
