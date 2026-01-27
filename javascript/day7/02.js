9a875ce4f7fe3bda3c188fe2bca9ace9fb1a0c0c
Tejas <tejaspawar8420@gmail.com>
2026-01-27 14:54:17 +0530

    switch case

---
+++ javascript/day7/02.js
@@ -0,0 +1,31 @@
+const monthNum = 5;
+
+switch (monthNum) {
+  case 1:
+    console.log("January");
+    break;
+
+  case 2:
+    console.log("feb");
+    break;
+
+  case 3:
+    console.log("march");
+    break;
+
+  case 4:
+    console.log("april");
+    break;
+
+  case 5:
+    console.log("May");
+    break;
+
+  case 6:
+    console.log("June");
+    break;
+
+  default:
+    console.log("default");
+    break;
+}
