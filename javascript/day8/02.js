581d8c56e5e5ae53de0149dcebde7462e6871cf5
Tejas <tejaspawar8420@gmail.com>
2026-01-27 16:42:48 +0530

    while and do while loop

---
+++ javascript/day8/02.js
@@ -0,0 +1,36 @@
+let index = 0;
+while (index <= 10) {
+  console.log(`value is ${index}`);
+  index++;
+}
+
+
+
+
+let myArr = ["Javascript", "React", "Java"];
+let arr = 0;
+while (arr <= myArr.length) {
+  console.log(`arr value is ${myArr[arr]}`);
+  arr = arr + 1;
+}
+
+
+
+
+//do while loop
+
+let score = 1
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score <=10);
+
+
+
+let score2 = 11
+do {
+    console.log(`score2 is ${score2}`);//11
+    score2++;
+} while (score2 <=10);
+
+
