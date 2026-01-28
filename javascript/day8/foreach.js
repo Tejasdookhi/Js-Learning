b718ec3258bcc1a69c711ce99e8a4b3958e6be1f
Tejas <tejaspawar8420@gmail.com>
2026-01-28 13:10:53 +0530

    .forEach

---
+++ javascript/day8/foreach.js
@@ -0,0 +1,42 @@
+// // //for each
+
+// let arr = [1, 2, 3, 4, 5];
+
+// arr.forEach((item) => {
+//   console.log(item); //1 2 3 4 5
+// });
+
+// // //----------------------------------------
+
+// let str = ["javascript", "java", "php"];
+
+// str.forEach((item) => {
+//   console.log(item); //"javascript", "java" ,"php"
+// });
+
+// //----------------------------------------
+
+// let obj = [
+//   {
+//     codinglang: "javascript",
+//     ext: "js",
+//   },
+//   {
+//     codinglang: "java",
+//     ext: "java",
+//   },
+//   {
+//     codinglang: "python",
+//     ext: "py",
+//   },
+// ];
+
+// obj.forEach( (item)=>{
+//     console.log(item.codinglang);   
+// }
+// )
+
+
+//-------------------------------------------------
+
+
