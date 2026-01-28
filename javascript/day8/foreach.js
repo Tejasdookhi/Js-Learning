5a9a56e3e3337076aadade32dedfbf4c132e55b3
Tejas <tejaspawar8420@gmail.com>
2026-01-28 13:11:26 +0530

    .forEach

--- javascript/day8/foreach.js
+++ javascript/day8/foreach.js
@@ -1,40 +1,40 @@
-// // //for each
+// //for each
 
-// let arr = [1, 2, 3, 4, 5];
+let arr = [1, 2, 3, 4, 5];
 
-// arr.forEach((item) => {
-//   console.log(item); //1 2 3 4 5
-// });
-
-// // //----------------------------------------
-
-// let str = ["javascript", "java", "php"];
-
-// str.forEach((item) => {
-//   console.log(item); //"javascript", "java" ,"php"
-// });
+arr.forEach((item) => {
+  console.log(item); //1 2 3 4 5
+});
 
 // //----------------------------------------
 
-// let obj = [
-//   {
-//     codinglang: "javascript",
-//     ext: "js",
-//   },
-//   {
-//     codinglang: "java",
-//     ext: "java",
-//   },
-//   {
-//     codinglang: "python",
-//     ext: "py",
-//   },
-// ];
+let str = ["javascript", "java", "php"];
 
-// obj.forEach( (item)=>{
-//     console.log(item.codinglang);   
-// }
-// )
+str.forEach((item) => {
+  console.log(item); //"javascript", "java" ,"php"
+});
+
+//----------------------------------------
+
+let obj = [
+  {
+    codinglang: "javascript",
+    ext: "js",
+  },
+  {
+    codinglang: "java",
+    ext: "java",
+  },
+  {
+    codinglang: "python",
+    ext: "py",
+  },
+];
+
+obj.forEach( (item)=>{
+    console.log(item.codinglang);   
+}
+)
 
 
 //-------------------------------------------------
