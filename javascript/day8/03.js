3d2dc75a0b046f4f31543c17e15c1210ce30e927
Tejas <tejaspawar8420@gmail.com>
2026-01-28 13:09:47 +0530

    forofloop

---
+++ javascript/day8/03.js
@@ -0,0 +1,26 @@
+// for of loop
+//for (anything of arr string or obj){log}
+
+
+let arr = [1, 2, 3, 4];
+
+for (const value of arr) {
+  console.log(value); //1 2 3 4 5
+}
+
+
+
+let arr2 = ["javascript", "java", "php"];
+for (const strings of arr2) {
+  console.log(`strings value are ${strings}`); //'javascript' , 'java', 'php'
+}
+
+
+
+let stringsTo = "javascript";
+for (const strings of stringsTo) {
+  console.log(`each value of string are ${strings}`); // j a v a s c r i p t each value will be print
+}
+
+
+
