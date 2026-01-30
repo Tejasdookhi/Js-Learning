10cce017488a1e4f6ddba6bf48e226d3c6a9850a
Tejas <tejaspawar8420@gmail.com>
2026-01-30 13:08:12 +0530

    for of loop

---
+++ javascript/day9/for(of)loop.js
@@ -0,0 +1,28 @@
+//for of loop :-
+
+let number = [1 , 2 , 3 , 4 , 5];
+for (const nums of number) {
+    console.log(nums);//12345   
+}
+
+//------------------------------------------------------
+
+let names = "java script"
+for(const name of names){
+    console.log(name);//java script   
+}
+
+//-------------------------------------------------------------
+
+let arrOfStr = ["Js" , "Java", "Python" , "Php" ]
+for (const ars of arrOfStr) {
+    console.log(ars);//Js  Java Python  Php  
+}
+
+//----------------------------------------------------------
+
+let obj =[{name:"java",age:80}]
+
+for (const objs of obj) {
+ console.log(objs);//{ name: 'java', age: 80 }    
+}
